f36f978464a414d6068b12eaa7c1addc1cc56d29
KauanFM <kauanmendes.5@hotmail.com>
2024-05-17 09:16:04 -0300

    Criação do projeto

---
+++ src/main/java/com/api/smd/apismd/ApiSmdApplication.java
@@ -0,0 +1,13 @@
+package com.api.smd.apismd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSmdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/smd/apismd/ApiSmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.smd.apismd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
